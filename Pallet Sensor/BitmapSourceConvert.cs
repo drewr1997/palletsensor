c3c01915213f518fabf7f1e83f3d02f91e0c37e5
drewr1997 <drewross1997@gmail.com>
2020-01-29 00:02:58 +0000

    1.0

---
+++ Pallet Sensor/BitmapSourceConvert.cs
@@ -0,0 +1,31 @@
+﻿using Emgu.CV.Structure;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+//Used to convert between Image<> and BitmapSource
+//Source: http://www.emgu.com/wiki/index.php/WPF_in_CSharp
+
+public static class BitmapSourceConvert
+{
+    [DllImport("gdi32")]
+    private static extern int DeleteObject(IntPtr o);
+
+    public static BitmapSource ToBitmapSource(Emgu.CV.Image<Hsv, Byte> image)
+    {
+        using (System.Drawing.Bitmap source = image.Bitmap)
+        {
+            IntPtr ptr = source.GetHbitmap();
+
+            BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                ptr,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+            DeleteObject(ptr);
+            return bs;
+        }
+    }
+}
